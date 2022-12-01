c8ed609e00cdc6d029af93a553944ba7151db071
dosaguz <xdg2001@gmail.com>
2022-12-01 19:12:16 +0100

    new file:   jamau.py

---
+++ jamau.py
@@ -0,0 +1,8 @@
+def eljamau():
+    p=input("Dale al enter sin nada o te grito: ")
+    if p=="":
+        print("YEYEYEYE")
+    else:
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+
+eljamau()
